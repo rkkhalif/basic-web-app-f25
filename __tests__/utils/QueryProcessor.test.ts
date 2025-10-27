9e4d7024fd506684e53dda67fa2d6c4d1c1688c5
raghd al-khalifa <raghd@dyn-172-20-102-127.qatar.cmu.local>
2025-10-27 12:00:36 +0300

    fix

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -15,14 +15,18 @@ describe("QueryProcessor", () => {
             "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
             "English poet, playwright, and actor, widely regarded as the greatest " +
             "writer in the English language and the world's pre-eminent dramatist."
-          ));
+        ));
     });
 
     test('should return name', () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "Rohan"
-          ));
-    })
+        expect(response).toBe("Rohan");
+    });
+    
+    test('should return largest', () => {
+        const query = "Which of the following numbers is the largest: 74, 43, 56?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe("74");
+    });
 });
